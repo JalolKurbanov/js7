393f80be06ee283e06885ebd308052bf5cd0686b
Jalol <jalolkurbanov11@gmail.com>
2023-09-23 16:38:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let a = +prompt('количество');
+let b = prompt('Символ отступов');
+let c = prompt('Конечный символ');
+let sum = "";
+
+for(let i = 1; i<=a;i++){
+    console.log(sum+c);
+    sum+=b;
+}
